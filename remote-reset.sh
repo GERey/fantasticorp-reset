ee2251f1fb036c7085e097aa5ef5dc9ba0e26c28
George <george.circleci.com>
2017-05-09 15:05:27 -0700

    changes to remote-reset

--- remote-reset.sh
+++ remote-reset.sh
@@ -5,6 +5,9 @@ set -e
 
 
 sudo docker exec -i $(sudo docker ps | grep circleci-frontend | awk '{print $1}') lein repl :connect 6005 <<EOF
+(in-ns 'circle.model.build-storage.core)
+(def count-by-mongo-opts (constantly 0))
+(def delete-archived-build! identity)
 (circle.http.api.admin-commands.user/force-unfollow "$GH_USER" "$GH_USER/$GH_REPO")
 (circle.http.api.admin-commands/delete-project "$GH_USER/$GH_REPO")
 (let [user (circle.model.user/find-one-by-login "$GH_USER") project (circle.model.api1/find-or-create-project "https://github.com/$GH_USER/$GH_REPO" :user user)] 
